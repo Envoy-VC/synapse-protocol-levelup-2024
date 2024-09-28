664e311e8a372bee51da95c23b19a81d57b48e6a
Vedant Chainani <vedantchainani1084@gmail.com>
2024-09-28 23:06:48 +0530

    chore: add conduit base contracts and common config

---
+++ packages/contracts/src/common/ConduitBase.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {IConduit} from "../interfaces/IConduit.sol";
+import {ISynapse} from "../interfaces/ISynapse.sol";
+
+contract ConduitBase is IConduit {
+    ISynapse public synapse;
+
+    error OnlySynapse();
+
+    constructor(address _synapse) {
+        synapse = ISynapse(_synapse);
+    }
+
+    modifier onlySynapse() {
+        if (msg.sender != address(synapse)) {
+            revert OnlySynapse();
+        }
+        _;
+    }
+}
---
+++ packages/contracts/src/conduits/Sudoku.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import {ConduitBase} from "../common/ConduitBase.sol";
+
+contract Sudoku is ConduitBase {
+    constructor(address _synapse) ConduitBase(_synapse) {}
+}
