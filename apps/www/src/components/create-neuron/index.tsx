783c168e64d4e9c47c3041e6bd1ca3d24bccf8a3
Vedant Chainani <vedantchainani1084@gmail.com>
2024-09-30 18:17:42 +0530

    chore: add neuron description

--- apps/www/src/components/create-neuron/index.tsx
+++ apps/www/src/components/create-neuron/index.tsx
@@ -16,14 +16,18 @@ export const CreateNeuron = () => {
         </div>
         <div className='flex h-full w-full basis-1/2 flex-col py-24'>
           <div className='my-24 flex flex-col gap-4 py-12'>
-            <div className='pb-12 font-eastman text-6xl text-[#DDE4FF]'>
+            <div className='pb-12 font-eastman text-6xl text-[#``DDE4FF``]'>
               Neurons
             </div>
             <p className='max-w-lg'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
-              quisquam quae quasi corrupti eum, exercitationem, necessitatibus
-              id officia saepe neque illum cum dignissimos eius beatae numquam
-              iusto quis, velit nobis!
+              In Synapse Protocol, neurons represent individual smart contracts
+              that serve as players&apos; interactive units within the network.
+              Each neuron can send and receive messages, triggering actions
+              within the game environment or forwarding messages to other
+              neurons, creating complex, decentralized interactions. Neurons
+              collectively manipulate the game state by influencing entropy and
+              stability, with each action contributing to either increasing
+              chaos or pushing the system toward equilibrium.{' '}
             </p>
             <CreateNeuronButton />
           </div>
