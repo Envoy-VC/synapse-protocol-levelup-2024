df4da796d64083d5c54d125794896f70514126d1
Vedant Chainani <vedantchainani1084@gmail.com>
2024-09-30 13:06:19 +0530

    chore: add mint logic

--- apps/www/src/app/conduits/stablecoin/_components/buy-box.tsx
+++ apps/www/src/app/conduits/stablecoin/_components/buy-box.tsx
@@ -1,8 +1,21 @@
+'use client';
+
 import Image from 'next/image';
 
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 
+import { errorHandler } from '~/lib/utils';
+import {
+  mockPriceFeedConfig,
+  stablecoinConduitConfig,
+  wagmiConfig,
+} from '~/lib/viem';
+
+import { waitForTransactionReceipt } from '@wagmi/core';
 import ETHLogo from 'public/eth-logo.png';
+import { toast } from 'sonner';
+import { parseEther } from 'viem';
+import { useAccount, useReadContract, useWriteContract } from 'wagmi';
 
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
@@ -10,6 +23,40 @@ import { Input } from '~/components/ui/input';
 import { ArrowDownIcon, BrainIcon } from 'lucide-react';
 
 export const BuyBox = () => {
+  const { address } = useAccount();
+  const [depositAmount, setDepositAmount] = useState<string>('');
+
+  const { data: currentPrice } = useReadContract({
+    ...mockPriceFeedConfig,
+    functionName: 'getCurrentPrice',
+  });
+
+  const { writeContractAsync, isPending } = useWriteContract();
+
+  const mintTokens = async () => {
+    const id = toast.loading('Minting tokens...');
+    try {
+      if (!address) {
+        throw new Error('Please connect to a wallet');
+      }
+      const amount = parseEther(depositAmount);
+
+      const hash = await writeContractAsync({
+        ...stablecoinConduitConfig,
+        functionName: 'mint',
+        value: amount,
+      });
+      await waitForTransactionReceipt(wagmiConfig, { hash });
+      toast.success('Minted tokens successfully', { id });
+    } catch (error) {
+      toast.error(errorHandler(error), { id });
+    }
+  };
+
+  const receiveAmount = useMemo(() => {
+    const price = Number(currentPrice ?? 0) / 10 ** 6;
+    return Number(depositAmount) * price;
+  }, [currentPrice, depositAmount]);
   return (
     <div className='flex h-fit w-full max-w-lg flex-col gap-3'>
       <div className='pb-6 font-eastman text-2xl text-[#DDE4FF]'>
@@ -28,6 +75,10 @@ export const BuyBox = () => {
               <Input
                 className='border-none bg-[#0f0f0f] px-0 text-4xl outline-none placeholder:text-4xl'
                 placeholder='0'
+                value={depositAmount}
+                onChange={(e) => {
+                  setDepositAmount(e.target.value);
+                }}
               />
               <div className='flex flex-row items-center gap-2 rounded-3xl bg-[#000] px-3 py-1 text-lg text-[#fff]'>
                 <div className='h-[1.5rem] w-[1.5rem]'>
@@ -43,6 +94,7 @@ export const BuyBox = () => {
               <Input
                 className='border-none bg-[#0f0f0f] px-0 text-4xl outline-none placeholder:text-4xl'
                 placeholder='0'
+                value={String(receiveAmount)}
               />
               <div className='flex flex-row items-center gap-2 rounded-3xl bg-[#000] px-3 py-1 text-lg text-[#DDE4FF]'>
                 <BrainIcon className='text-[#DDE4FF]' size={24} />
@@ -53,7 +105,9 @@ export const BuyBox = () => {
         </div>
         <Button
           className='h-12 !rounded-3xl bg-[#c4d0ff] text-lg font-medium text-[#5e7eff] hover:bg-[#b8c6fe] hover:text-[#5e7eff]'
+          disabled={isPending}
           variant='ghost'
+          onClick={mintTokens}
         >
           Mint
         </Button>
